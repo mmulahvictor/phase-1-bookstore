8fe6034e4afbff2e1300c9c6ed67592ccb814165
mmulahvictor <mmulahvictor@gmail.com>
2022-06-21 19:00:02 +0300

    fetch data from db.json in JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,4 @@
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '6b1f82b533msh87ecd04ffcee0f1p12cea3jsn1b1eb3217562',
-		'X-RapidAPI-Host': 'bookshelves.p.rapidapi.com'
-	}
-};
-
-fetch('https://bookshelves.p.rapidapi.com/books', options)
+fetch('http://localhost:3000/Books')
 	.then(response => response.json())
-	.then(response => console.log(response))
+	.then(data => console.log(data))
 	.catch(err => console.error(err));
