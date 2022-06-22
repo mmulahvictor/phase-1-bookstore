599deb17ff70435adc839e3e7dad585382329d5d
mmulahvictor <mmulahvictor@gmail.com>
2022-06-22 14:53:17 +0300

    add book details

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,23 @@
-fetch('http://localhost:3000/Books')
+let url = 'http://localhost:3000/Books'
+
+fetch(url)
 	.then(response => response.json())
-	.then(data => console.log(data))
-	.catch(err => console.error(err));
+	.then(data => {
+		let html = ""
+		data.map(value => {
+			// console.log(value.price)
+			html += `
+				<div class="col-md-4">
+                <div class="card" style="width:400px">
+                    <img class="card-img-top" src= ${value.imgUrl} style="width:100%">
+                    <div class="card-body">
+                        <h4 class="card-title">J${value.title}</h4>
+                        <p class="card-text">${value.description}</p>
+                        <a href="#" class="btn btn-primary">${value.price}</a>
+                    </div>
+                </div>
+            </div>
+			`
+		})
+		document.getElementById('row').innerHTML = html
+	})
