81c16eb8e31dd107d0acb3f51433af44d8512bf4
mmulahvictor <mmulahvictor@gmail.com>
2022-06-24 02:52:05 +0300

    resizing the books cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,13 +20,13 @@ fetch(url)
 
 			p += `
 				<div class="col-md-4 rem">
-                <div class="card" style="width:400px">
-                    <img class="card-img-top" src= ${book.imgUrl} style="width:100%">
-                    <div class="card-body">
-                        <h4 class="card-title text-danger">J${book.title}</h4>
-                        <p class="card-text">${book.description}</p>
-                        <a href="#" class="btn btn-danger">${book.price}</a>
-                    </div>
+                	<div class="card" style="width:400px">
+                	    <img class="card-img-top" src= ${book.imgUrl} style="width:100%">
+                	    <div class="card-body">
+                	        <h4 class="card-title text-danger">J${book.title}</h4>
+                	        <p class="card-text">${book.description}</p>
+                	        <a href="#" class="btn btn-danger">${book.price}</a>
+                	    </div>
                 	</div>
             	</div>
 			`
