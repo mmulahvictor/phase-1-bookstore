ebe0c18503131d4ae47dfcc6a918b59e8b62f28f
mmulahvictor <mmulahvictor@gmail.com>
2022-06-24 02:21:48 +0300

    styling and added some interactivity on js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,70 @@
 let url = 'http://localhost:3000/Books'
 const searchInput = document.querySelector("[data-search]")
+let p = ""
+const input = document.querySelector('search')
+const searchBtn = document.querySelector('#search-btn');
+// const resultHeading = document.querySelector('#result-heading');
+// const cocktailsElement = document.querySelector('#cocktails');
 
-let users = []
-
-searchInput.addEventListener("input", (e) => {
-	const value = e.target.value
-	// users.forEach(data =>{
-	// 	const isVisible = data.title.includes(value) || data.author.includes(value)
-	// 	data.element.classList.toggle("hide", !isVisible)
-		console.log(users)
-	// })
-})
-
+function fetchBooks(){
 fetch(url)
 	.then(response => response.json())
 	.then(data => {
+	
+	data.forEach(book =>{
+		
+		let divc = document.createElement('div')
+		divc.className = "created"
+		divc.innerHTML = p
+		document.querySelector('#row').appendChild(divc)
 
-		let html = ""
-		users = data.map(value => {
-			// console.log(value.price)
-			html += `
-				<div class="col-md-4">
+			p += `
+				<div class="col-md-4 rem">
                 <div class="card" style="width:400px">
-                    <img class="card-img-top" src= ${value.imgUrl} style="width:100%">
+                    <img class="card-img-top" src= ${book.imgUrl} style="width:100%">
                     <div class="card-body">
-                        <h4 class="card-title text-danger">J${value.title}</h4>
-                        <p class="card-text">${value.description}</p>
-                        <a href="#" class="btn btn-danger">${value.price}</a>
+                        <h4 class="card-title text-danger">J${book.title}</h4>
+                        <p class="card-text">${book.description}</p>
+                        <a href="#" class="btn btn-danger">${book.price}</a>
                     </div>
-                </div>
-            </div>
+                	</div>
+            	</div>
 			`
-			return { title: data.title, author: data.author, element: html }
-		})
-		document.getElementById('row').innerHTML = html
+
+			// searchBtn.addEventListener("click", getBooks) 
+			// 	const getBooks = () =>{
+			// 		input.value.trim();
+			// 		fetch(`${url}${input}`)
+			// 		.then(resp => resp.json())
+			// 		.then(data =>{
+			// 			console.log(data)
+			// 			if(data.title){
+			// 				data.title.forEach(title =>{
+			// 					p += `
+			// 	<div class="col-md-4 rem">
+            //     <div class="card" style="width:400px">
+            //         <img class="card-img-top" src= ${book.imgUrl} style="width:100%">
+            //         <div class="card-body">
+            //             <h4 class="card-title text-danger">J${book.title}</h4>
+            //             <p class="card-text">${book.description}</p>
+            //             <a href="#" class="btn btn-danger">${book.price}</a>
+            //         </div>
+            //     	</div>
+            // 	</div>
+			// `
+			// 				})
+			// 			}
+			// 		})
+			// 	}
+			// 	const value = e.target.value.toLowerCase()
+				
+				// if (title.includes(value)) {
+				// 	divc.style.color = "red"
+				// }else{
+				// 	// div.created.className = "hidden"
+				// }
+			
 	})
+	document.querySelector('#row').innerHTML = p
+})}
+	fetchBooks()
